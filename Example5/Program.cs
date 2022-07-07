a2bb23965a9f963e9af35274715d2ecf1c0a0d1c
P1k4sh <p1k4sh@yandex.ru>
2022-07-07 14:42:38 +0300

    made new folder Example5 with Condition If_Else

---
+++ Example5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToUpper() == "MASHA"){
+    Console.WriteLine("Hey! There is MASHA!");
+} else {
+    Console.WriteLine("Hey, usual user " + username + "!");
+}
